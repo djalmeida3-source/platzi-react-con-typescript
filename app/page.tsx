2dabd8b4eb1eb08396e7a43417b5a8911766d0d9
djalmeida3 <djalmeida3@gmail.com>
2024-02-12 17:07:03 -0500

    State primitive data

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,27 @@
+'use client';
 import { RandomFox } from "@/components/RandomFox";
+import { useState } from "react";
 
 // Generate a ramdom function between 1 and 123
 const getRandomNumber = (): number => Math.floor(Math.random() * 123) + 1;
 
 export default function Home() {
+  const [images, setImages] = useState<string[]>([
+    `https://randomfox.ca/images/${getRandomNumber()}.jpg`,
+    `https://randomfox.ca/images/${getRandomNumber()}.jpg`,
+    `https://randomfox.ca/images/${getRandomNumber()}.jpg`,
+    `https://randomfox.ca/images/${getRandomNumber()}.jpg`,
+  ]);
+
   return (
     <main className="flex min-h-screen flex-col items-center p-14">
       <h1 className="text-3xl font-bold">Hello World</h1>
-      <RandomFox image={`https://randomfox.ca/images/${getRandomNumber()}.jpg`}/>
+      {
+        images.map((image, index) => (
+          <div key={index} className="p-4">
+            <RandomFox image={image}/>
+          </div>
+      ))}
     </main>
   );
 }
